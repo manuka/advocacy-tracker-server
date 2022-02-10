035437ed0608868cc1a1b7a2b626205161520e7f
Philip Arndt <git@p.arndt.io>
2022-02-10 20:01:20 +1300

    errors when the measure is the same as the other_measure

--- app/models/measure_measure.rb
+++ app/models/measure_measure.rb
@@ -5,4 +5,12 @@ class MeasureMeasure < VersionedRecord
   validates :measure_id, uniqueness: {scope: :other_measure_id}
   validates :measure_id, presence: true
   validates :other_measure_id, presence: true
+
+  validate :measure_not_other_measure
+
+  private
+
+  def measure_not_other_measure
+    errors.add(:measure, "can't be the same as other_measure") if measure == other_measure
+  end
 end
