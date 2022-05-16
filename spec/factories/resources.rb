9080b49b19dc7d55162e4e27a9d042f48290f756
Philip Arndt <git@p.arndt.io>
2022-05-16 21:51:41 +1200

    Fix resource factory being private by default

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -4,10 +4,17 @@ FactoryBot.define do
     association(:resourcetype)
     description { Faker::Movies::StarWars.quote }
     draft { false }
-    private { true }
     publication_date { Date.today }
     status { Faker::Creature::Cat.registry }
     title { "MyString" }
     url { "https://impactoss.org" }
+
+    trait :not_private do
+      private { false }
+    end
+
+    trait :private do
+      private { true }
+    end
   end
 end
