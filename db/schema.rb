71310040f2dc82714890f62b588c637245cf26fc
Philip Arndt <git@p.arndt.io>
2022-05-16 20:20:00 +1200

    Add private to indicators, defaulting to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_080912) do
+ActiveRecord::Schema.define(version: 2022_05_16_081829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,6 +181,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_080912) do
     t.string "reference"
     t.integer "updated_by_id"
     t.integer "created_by_id"
+    t.boolean "private", default: false
     t.index ["created_at"], name: "index_indicators_on_created_at"
     t.index ["draft"], name: "index_indicators_on_draft"
     t.index ["manager_id"], name: "index_indicators_on_manager_id"
