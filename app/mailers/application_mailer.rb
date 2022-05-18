2582a973b67a1ee9112c67a04dd9b7eaf44f5f7f
tmfrnz <timo@dumpark.com>
2022-05-18 10:02:39 +0200

    update temp email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@mail.impactoss.org"
+  default from: "no-reply@dumpark.com"
   layout "mailer"
 end
