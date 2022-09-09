2577505752ca2d6bbce16ca7477a9c599dd10837
tmfrnz <timo@dumpark.com>
2022-09-09 18:13:50 +0200

    one more update, trying to specify the return address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@
 
 class ApplicationMailer < ActionMailer::Base
   default from: "Global Plastic Policy Team <plasticpolicy@wwf.no>",
-          return_path: "Global Plastic Policy Team <no-reply-plasticpolicy@wwf.no>"
+          to: "Global Plastic Policy Team <no-reply-plasticpolicy@wwf.no>"
   layout "mailer"
 end
