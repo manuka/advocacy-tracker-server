1a6e49ddf0827bfdacce2da52865ffb40aae75f3
Philip Arndt <git@p.arndt.io>
2022-09-13 20:29:17 +1200

    Add spec for the notifications default of false

--- spec/models/measuretype_spec.rb
+++ spec/models/measuretype_spec.rb
@@ -3,4 +3,8 @@ require "rails_helper"
 RSpec.describe Measuretype, type: :model do
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to have_many :measures }
+
+  it "is expected to default notifications to false" do
+    expect(subject.notifications).to eq(false)
+  end
 end
